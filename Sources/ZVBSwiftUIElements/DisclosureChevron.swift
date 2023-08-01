ae3baccba11c13825d8bd52c7eff21c01b0e8900
Zoe Van Brunt <zoe@zoelog.io>
2023-07-31 19:00:38 -0700

    Remove DisclosureChevronView UIKit version

--- Sources/ZVBSwiftUIElements/DisclosureChevron.swift
+++ Sources/ZVBSwiftUIElements/DisclosureChevron.swift
@@ -21,50 +21,11 @@ public struct DisclosureChevron: View {
     }
 }
 
-@available(iOS 13.0, *)
-class DisclosureChevronView: UIView {
-    var insets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 8)
-        
-    lazy var imageView = UIImageView(image: .chevron)
-        .apply {
-            $0.contentMode = .right
-        }
-        .withTintColor(.tertiaryLabel)
-        .subview(of: self)
-    
-    override var intrinsicContentSize: CGSize {
-        return sizeThatFits(.max)
-    }
-    
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
-        return imageView.sizeThatFits(size).expanded(by: insets)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        imageView.frame = bounds.inset(by: insets)
-    }
-}
-
-@available(iOS 13.0, *)
-extension UIImage {
-    static var chevron: UIImage? {
-        UIImage(systemName: "chevron.right", withConfiguration: UIImage.SymbolConfiguration(
-            font: UIFontMetrics(forTextStyle: .caption1)
-                .scaledFont(for: .systemFont(ofSize: 12, weight: .bold)))
-        )
-    }
-}
-
 @available(iOS 15.0, *)
 struct DisclosureChevron_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             DisclosureChevron()
-            UIViewPreview {
-                DisclosureChevronView()
-            }
         }
         .padding()
         .previewLayout(.sizeThatFits)
